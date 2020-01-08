48838dac5ec4c101887eb3e9ce7dd295a94393d5
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-08 05:18:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,37 @@
-# My Code here....
+# map-like
+
+def map_to_negativize(source_array)
+  
+end  
+
+def map_to_no_change(source_array)
+  index = 0 
+  while index < source_array.length do
+    source_array[index] * -1
+    index += 1
+  end
+  source_array
+end
+
+def map_to_double(source_array)
+  
+end
+
+def map_to_square(source_array)
+  
+end
+
+# reduce-like
+
+def reduce_to_total(source_array, starting_point)
+  
+end  
+
+def reduce_to_all_true(source_array)
+  
+end
+
+def reduce_to_any_true(source_array)
+  
+end
+
