9a9056bc742519787bb962bb0fd4ce85cef649ef
Christopher Wagstaffe <cjwagstaffe@gmail.com>
2020-01-08 05:57:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -37,6 +37,11 @@ def reduce_to_total(source_array, starting_point)
   if starting_point == []
     starting_point = 0
   end
+  index = 0 
+  while index < source_array.length do
+    starting_point += source_array[index]
+    index += 1
+  end
 end  
 
 def reduce_to_all_true(source_array)
